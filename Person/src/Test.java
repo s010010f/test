30ede8c5765cdad388c200eab122654d80410bbb
s010010f <s.010010f.f@gmail.com>
2019-03-15 20:49:11 +0900

    20190315

---
+++ Person/src/Test.java
@@ -0,0 +1,57 @@
+
+public class Test {
+
+	public static void main(String[] args){
+		Person taro = new Person();
+		taro.name = "山田太郎";
+		taro.age = 20;
+		taro.phoneNumber = "00011112222";
+		taro.address = "aaa";
+
+		Person jiro = new Person();
+		jiro.name = "木村次郎";
+		jiro.age = 18;
+		jiro.phoneNumber = "33344445555";
+		jiro.address = "bbb";
+
+		Person hanako = new Person();
+		hanako.name = "鈴木花子";
+		hanako.age = 16;
+		hanako.phoneNumber = "66677778888";
+		hanako.address = "ccc";
+
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		System.out.println(jiro.phoneNumber);
+		System.out.println(jiro.address);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		System.out.println(hanako.phoneNumber);
+		System.out.println(hanako.address);
+
+		taro.talk();
+		jiro.walk();
+		hanako.run();
+
+
+		Robot aibo = new Robot();
+		aibo.name = "aibo";
+
+		Robot asimo = new Robot();
+		asimo.name = "asimo";
+
+		Robot pepper = new Robot();
+		pepper.name = "pepper";
+
+		aibo.talk();
+		asimo.walk();
+		pepper.run();
+
+
+	}
+
+}
