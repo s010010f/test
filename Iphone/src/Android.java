ad1ca0462e299549e4f30773b11955e647348759
unknown <S12@aol.jp>
2019-04-04 16:11:05 +0900

    2019/04/04 Javaオブジェクト指向演習終了

---
+++ Iphone/src/Android.java
@@ -0,0 +1,18 @@
+
+public class Android extends SmartPhone{
+
+	public static void main(String[] args) {
+
+		SmartPhone android = new SmartPhone();
+		android.play();
+		android.stop();
+		android.next();
+		android.back();
+		android.call();
+		android.mail();
+		android.photo();
+		android.internet();
+
+	}
+
+}
