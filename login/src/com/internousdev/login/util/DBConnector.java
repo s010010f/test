dd50d57cd6311757073babefd23980bd8d25635b
unknown <S12@aol.jp>
2019-04-17 20:38:07 +0900

    2019/04/17 ログイン認証まで

--- login/src/com/internousdev/login/util/DBConnector.java
+++ login/src/com/internousdev/login/util/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/login_fukuoka";
+	private static String url = "jdbc:mysql://localhost/logindb_fukuoka";
 
 	private static String user = "root";
 	private static String password = "";
